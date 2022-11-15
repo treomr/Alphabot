1705c8f3de5c42a2ca4df708000c302e332f47ad
treomr <108604333+treomr@users.noreply.github.com>
2022-11-15 13:36:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.APIs = {
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz' : 'your key', // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz' : 'ppyfQa5z', // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 global.self_mode = false
@@ -16,21 +16,21 @@ global.autobio = true
 global.msg_connected = false
 global.user_register = false
 global.anti_delete = false
-global.anticall = false
+global.anticall = true
 global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "6285823265582"
+global.ownerbot = ['6285823265582@s.whatsapp.net','6285823265582@s.whatsapp.net'] // .owner
+global.ownername = "ᴹᴿ᭄ ͓̽M͓̽a͓͓̽̽n͓̽ ͓̽M͓͓̽̽D͓̽ ×፝֟͜×"
+global.botname = "Bʅαƈƙ Sԋαɾƙ"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  ALPHABOT  」\nCreated By Bʅαƈƙ Sԋαɾƙ"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
